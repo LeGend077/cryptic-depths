7caab655f8d8fda5b9fbea7ff791b4e4e4022dd4
LeGend <98607285+LeGend077@users.noreply.github.com>
2025-03-13 00:05:04 +0530

    backup-7
    soul altar rework and redesign/recode

--- BP/scripts/customComponents.js
+++ BP/scripts/customComponents.js
@@ -39,42 +39,14 @@ function runeParticles(block) {
 }
 
 /** @type {import("@minecraft/server").BlockCustomComponent} */
-const spawnParticleBlockComponent = {
-    onTick(event) {
-        const { x, y, z } = event.block.location
-        event.block.dimension.spawnParticle("minecraft:blue_flame_particle",
-            {
-                x: x,
-                y: y + (9 / 16),
-                z: z
-            }
-        );
-        event.block.dimension.spawnParticle("minecraft:blue_flame_particle",
-            {
-                x: x + 1,
-                y: y + (9 / 16),
-                z: z
-            }
-        );
-        event.block.dimension.spawnParticle("minecraft:blue_flame_particle",
-            {
-                x: x,
-                y: y + (9 / 16),
-                z: z + 1
-            }
-        );
-        event.block.dimension.spawnParticle("minecraft:blue_flame_particle",
-            {
-                x: x + 1,
-                y: y + (9 / 16),
-                z: z + 1
-            }
-        );
-    }
-}
-
-/** @type {import("@minecraft/server").BlockCustomComponent} */
-const applyEnchantBlockComponent = {
+const soulAltarBlockComponent = {
+    onPlace(event) {
+        event.block.setPermutation(event.block.permutation.withState('cryptic_depths:activated', false))
+        event.block.setPermutation(event.block.permutation.withState('cryptic_depths:currentSelectedItemInAltar', 'none'))
+    },
+    // onTick(event) {
+    //     const { x, y, z } = event.block.location
+    // },
     onPlayerInteract(event) {
         const player = event.player;
         const block = event.block;
@@ -83,55 +55,64 @@ const applyEnchantBlockComponent = {
         /** @type {import("@minecraft/server").EntityEquippableComponent} */
         const equipment = player.getComponent("minecraft:equippable");
         const mainHandItem = equipment.getEquipment(EquipmentSlot.Mainhand);
+        const candleCondition = (mainHandItem?.typeId === 'minecraft:candle' && mainHandItem?.amount === 4);
 
-        if (mainHandItem?.typeId === 'cryptic_depths:soul_mending' && block.permutation.getState('cryptic_depths:currentSelectedItemInAltar') === 'none') {
-            block.setPermutation(block.permutation.withState("cryptic_depths:currentSelectedItemInAltar", 'cryptic_depths:soul_mending'));
-
+        if (block.permutation.getState('cryptic_depths:activated') === false && candleCondition) {
+            block.setPermutation(block.permutation.withState('cryptic_depths:activated', true))
             equipment.setEquipment(EquipmentSlot.Mainhand, undefined)
-
-            runeParticles(block)
-            runeParticles(block)
-            runeParticles(block)
-            const varMap = new MolangVariableMap();
-            varMap.setVector3('direction', { x: 0, y: 1, z: 0 });
-
-            player.playSound('bloom.sculk_catalyst')
-            block.dimension.spawnParticle("minecraft:soul_particle", {
-                x: x + 0.5,
-                y: y + (9 / 16),
-                z: z + 0.5
-            }, varMap);
+        } else if (block.permutation.getState('cryptic_depths:activated') === false && !candleCondition) {
+            player.onScreenDisplay.setActionBar('Activate the Soul Altar using 4 candles before using it!')
         }
-        if (block.permutation.getState('cryptic_depths:currentSelectedItemInAltar') === 'cryptic_depths:soul_mending' && !(mainHandItem?.typeId === 'cryptic_depths:soul_mending') && !mainHandItem?.typeId.includes('_sword')) {
+        else if (block.permutation.getState('cryptic_depths:activated') === true) {
+            if (mainHandItem?.typeId === 'cryptic_depths:soul_mending' && block.permutation.getState('cryptic_depths:currentSelectedItemInAltar') === 'none') {
+                block.setPermutation(block.permutation.withState("cryptic_depths:currentSelectedItemInAltar", 'cryptic_depths:soul_mending'));
 
-            block.setPermutation(block.permutation.withState("cryptic_depths:currentSelectedItemInAltar", 'none'));
-            player.dimension.spawnItem(new ItemStack("cryptic_depths:soul_mending", 1), player.location)
-        }
+                equipment.setEquipment(EquipmentSlot.Mainhand, undefined)
 
-        if (block.permutation.getState('cryptic_depths:currentSelectedItemInAltar') === 'cryptic_depths:soul_mending' && mainHandItem?.typeId.includes('_sword')) {
+                runeParticles(block)
+                runeParticles(block)
+                runeParticles(block)
+                const varMap = new MolangVariableMap();
+                varMap.setVector3('direction', { x: 0, y: 1, z: 0 });
 
-            if (player.getDynamicProperty('soul_energy') >= 10) {
-                block.setPermutation(block.permutation.withState("cryptic_depths:currentSelectedItemInAltar", 'none'));
-                block.dimension.spawnParticle("cryptic_depths:enchantment_magic_particle", {
+                player.playSound('bloom.sculk_catalyst')
+                block.dimension.spawnParticle("minecraft:soul_particle", {
                     x: x + 0.5,
                     y: y + (9 / 16),
                     z: z + 0.5
-                });
+                }, varMap);
+            }
+            if (block.permutation.getState('cryptic_depths:currentSelectedItemInAltar') === 'cryptic_depths:soul_mending' && !(mainHandItem?.typeId === 'cryptic_depths:soul_mending') && !mainHandItem?.typeId.includes('_sword')) {
 
-                const new_energy = player.getDynamicProperty('soul_energy') - 10
-                player.setDynamicProperty('soul_energy', new_energy)
+                block.setPermutation(block.permutation.withState("cryptic_depths:currentSelectedItemInAltar", 'none'));
+                player.dimension.spawnItem(new ItemStack("cryptic_depths:soul_mending", 1), player.location)
+            }
 
-                const item = mainHandItem.clone()
-                item.setLore([`${enchantments.soul_mending.id}§r§7Soul Mending§r`])
-                equipment.setEquipment(EquipmentSlot.Mainhand, item)
+            if (block.permutation.getState('cryptic_depths:currentSelectedItemInAltar') === 'cryptic_depths:soul_mending' && mainHandItem?.typeId.includes('_sword')) {
 
-                player.playSound('insert_enchanted.chiseled_bookshelf')
+                if (player.getDynamicProperty('soul_energy') >= 10) {
+                    block.setPermutation(block.permutation.withState("cryptic_depths:currentSelectedItemInAltar", 'none'));
+                    block.dimension.spawnParticle("cryptic_depths:enchantment_magic_particle", {
+                        x: x + 0.5,
+                        y: y + (9 / 16),
+                        z: z + 0.5
+                    });
 
-                player.onScreenDisplay.setTitle(`cryptic_depths:${player.getDynamicProperty("soul_energy")}`);
-            } else {
-                player.onScreenDisplay.setActionBar(
-                    'Atleast 50% soul energy is required to enchant!'
-                )
+                    const new_energy = player.getDynamicProperty('soul_energy') - 10
+                    player.setDynamicProperty('soul_energy', new_energy)
+
+                    const item = mainHandItem.clone()
+                    item.setLore([`${enchantments.soul_mending.id}§r§7Soul Mending§r`])
+                    equipment.setEquipment(EquipmentSlot.Mainhand, item)
+
+                    player.playSound('insert_enchanted.chiseled_bookshelf')
+
+                    player.onScreenDisplay.setTitle(`cryptic_depths:${player.getDynamicProperty("soul_energy")}`);
+                } else {
+                    player.onScreenDisplay.setActionBar(
+                        'Atleast 50% soul energy is required to enchant!'
+                    )
+                }
             }
         }
     }
@@ -157,10 +138,7 @@ const toolDurabilityComponent = {
 
 world.beforeEvents.worldInitialize.subscribe(({ blockComponentRegistry, itemComponentRegistry }) => {
     blockComponentRegistry.registerCustomComponent(
-        "cryptic_depths:spawn_particle", spawnParticleBlockComponent
-    );
-    blockComponentRegistry.registerCustomComponent(
-        "cryptic_depths:apply_enchant", applyEnchantBlockComponent
+        "cryptic_depths:soulAltarBlockComponent", soulAltarBlockComponent
     );
     itemComponentRegistry.registerCustomComponent('cryptic_depths:tool_durability', toolDurabilityComponent)
 })
